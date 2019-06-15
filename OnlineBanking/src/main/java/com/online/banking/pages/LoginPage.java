3c69c0c591ea07dade43d92c734a10a8e963c654
rashmijaingithub <49267291+rashmijaingithub@users.noreply.github.com>
2019-06-15 12:20:25 +0530

    additional files

--- OnlineBanking/src/main/java/com/online/banking/pages/LoginPage.java
+++ OnlineBanking/src/main/java/com/online/banking/pages/LoginPage.java
@@ -8,85 +8,72 @@ import org.testng.Assert;
 
 import com.online.banking.helper.BaseClass;
 
+public class LoginPage extends BaseClass {
 
+	// Login page
 
-public class LoginPage extends BaseClass{
-	
-	//Login page 
-	
-	  @FindBy(xpath=("//*[@id='HyperLink1']/img")) private static WebElement logo ;
-	  
-	  @FindBy(xpath="//a[@id='AccountLink']") private static WebElement
-	  ONLINEBANKINGTAB ;
-	  
-	  @FindBy(xpath="//a[@id='LinkHeader2']") private static WebElement Persnaltab
-	  ;
-	  
-	  @FindBy(xpath="//a[@id='LinkHeader3']") private static WebElement
-	  smallbusiness ;
-	  
-	  @FindBy(xpath="//a[@id='LinkHeader4']") private static WebElement INSIDEALTO;
-	 
+	@FindBy(xpath = ("//*[@id='HyperLink1']/img"))
+	private static WebElement logo;
 
+	@FindBy(xpath = "//a[@id='AccountLink']")
+	private static WebElement ONLINEBANKINGTAB;
 
-	
-	@FindBy(xpath="//input[@id='uid']")
-	private static WebElement username ; 
-	
-	@FindBy(xpath="//input[@id='passw']")
-	private static WebElement password ; 
-	
-	@FindBy (xpath="//*[@value='Login']")
-	private static WebElement signIn ;
-	
-	@FindBy(xpath="//span[@id='_ctl0__ctl0_Content_Main_message']")
-	private static WebElement ErrorMessage ;
-	
-	
-	
-public static void setUsername(String value)
-{
-	sendKeys(username,value);
-}
+	@FindBy(xpath = "//a[@id='LinkHeader2']")
+	private static WebElement Persnaltab;
 
-public static void setPassword(String pwd)
-{
-	sendKeys(password,pwd);
-}
+	@FindBy(xpath = "//a[@id='LinkHeader3']")
+	private static WebElement smallbusiness;
 
-public static void clickSignIn()
-{
-	signIn.click();
-}
-public void loginSuccessValidation(WebDriver driver) throws InterruptedException
-{
-	Thread.sleep(4000);
-	Assert.assertTrue(driver.getTitle().contains("Altoro Mutual"));
-	
-}
+	@FindBy(xpath = "//a[@id='LinkHeader4']")
+	private static WebElement INSIDEALTO;
 
-public static void loginpageValidations() throws InterruptedException
-{
-	
-	
-	Thread.sleep(4000);
-	
-		
-		 Assert.assertTrue(logo.isDisplayed(),"Logo not displaing");
-		
-		  Assert.assertTrue(ONLINEBANKINGTAB.isDisplayed(), "ONLINEBanking tab displayed");
-		  Assert.assertTrue(Persnaltab.isDisplayed(), "Persnaltab is displayed");
-		  Assert.assertTrue(smallbusiness.isDisplayed(), "small business tab is displayed");
-		  Assert.assertTrue(INSIDEALTO.isDisplayed(), "INSIDEALTO tab is displayed");
-		 
-		 
-}
-public static void errormessageValidation()
-{
-	
-	Assert.assertTrue(ErrorMessage.isDisplayed());
-	
-}
+	@FindBy(xpath = "//input[@id='uid']")
+	private static WebElement username;
 
+	@FindBy(xpath = "//input[@id='passw']")
+	private static WebElement password;
+
+	@FindBy(xpath = "//*[@value='Login']")
+	private static WebElement signIn;
+
+	@FindBy(xpath = "//span[@id='_ctl0__ctl0_Content_Main_message']")
+	private static WebElement ErrorMessage;
+
+	public static void setUsername(String value) {
+		sendKeys(username, value);
+	}
+
+	public static void setPassword(String pwd) {
+		sendKeys(password, pwd);
+	}
+
+	public static void clickSignIn() {
+		signIn.click();
+	}
+
+	public void loginSuccessValidation(WebDriver driver) throws InterruptedException {
+		Thread.sleep(4000);
+		Assert.assertTrue(driver.getTitle().contains("Altoro Mutual"));
+
+	}
+
+	public static void loginpageValidations() throws InterruptedException {
+
+		Thread.sleep(4000);
+
+		Assert.assertTrue(logo.isDisplayed(), "Logo not displaing");
+
+		Assert.assertTrue(ONLINEBANKINGTAB.isDisplayed(), "ONLINEBanking tab displayed");
+		Assert.assertTrue(Persnaltab.isDisplayed(), "Persnaltab is displayed");
+		Assert.assertTrue(smallbusiness.isDisplayed(), "small business tab is displayed");
+		Assert.assertTrue(INSIDEALTO.isDisplayed(), "INSIDEALTO tab is displayed");
+
+	}
+
+	public static void errormessageValidation() {
+
+		Assert.assertTrue(ErrorMessage.isDisplayed());
+
+	}
 
 }
